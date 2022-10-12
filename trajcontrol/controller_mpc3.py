8cbf7c5e10a0fa6cb00220ff5aea27b03a979b9b
maribernardes <maribernardes@gmail.com>
2022-10-12 10:51:13 -0400

    Updated available Jacobians to avoid confusion

--- trajcontrol/controller_mpc3.py
+++ trajcontrol/controller_mpc3.py
@@ -264,9 +264,9 @@ class ControllerMPC3(Node):
             self.cmd[2] = self.stage[2]
             u = np.array([[self.stage[0], self.stage[2]]])
 
-        # TO MAKE INSERTIONS WITHOUT COMPENSATION (DELETE AFTER)
-        self.cmd[0] = self.stage_initial[0]
-        self.cmd[2] = self.stage_initial[2]
+        # # TO MAKE INSERTIONS WITHOUT COMPENSATION (DELETE AFTER)
+        # self.cmd[0] = self.stage_initial[0]
+        # self.cmd[2] = self.stage_initial[2]
     
         # Print values
         self.get_logger().info('Applying trajectory compensation... DO NOT insert the needle now\nTip: (%f, %f, %f) \
